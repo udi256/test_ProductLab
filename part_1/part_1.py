b9f1af3055f8aa6739e13ec89d763a2d61310a66
udi256 <udi256@gmail.com>
2023-03-03 16:09:37 +0300

    refactoring

--- part_1/part_1.py
+++ part_1/part_1.py
@@ -2,18 +2,22 @@ import networkx as nx
 
 
 def check_relation(net, first, second):
-    g = nx.Graph()
-    for connect in net:
-        g.add_nodes_from(connect)
-        
-    g.add_edges_from(net)
     try:
         return True if nx.dijkstra_path_length(g,first,second) else False
 
     except nx.exception.NetworkXNoPath:
         return False
+    
 
-if __name__ == '__main__':
+def generete_network(net):
+    g = nx.Graph()
+    for connect in net:
+        g.add_nodes_from(connect)
+
+    g.add_edges_from(net)
+    return g
+
+def main():
     net = (
     ("Ваня", "Лёша"), ("Лёша", "Катя"),
     ("Ваня", "Катя"), ("Вова", "Катя"),
@@ -21,7 +25,9 @@ if __name__ == '__main__':
     ("Стёпа", "Оля"), ("Оля", "Настя"),
     ("Настя", "Дима"), ("Дима", "Маша")
     )
-
+    global g #объявлено глобальной т.к. необходимо использовать в функции список параметрой которой определен условием
+    # задачи. и вынесено из функции для однократной генерации и дальнейшего переиспользования
+    g = generete_network(net)
 
     assert check_relation(net, "Петя", "Стёпа") is True
     assert check_relation(net, "Маша", "Петя") is True
@@ -30,3 +36,6 @@ if __name__ == '__main__':
     assert check_relation(net, "Стёпа", "Маша") is True
     assert check_relation(net, "Лена", "Маша") is False
     assert check_relation(net, "Вова", "Лена") is True
+
+if __name__ == '__main__':
+    main()
